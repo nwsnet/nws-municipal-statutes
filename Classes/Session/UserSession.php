759c61f05625d89ed4c7e7ba03897a16c7c2e75a
Dirk Meinke <dmeinke@die-netzwerkstatt.de>
2019-04-05 07:49:16 +0200

    [TASK] Needed session features adjusted and unneeded deleted

--- Classes/Session/UserSession.php
+++ Classes/Session/UserSession.php
@@ -32,7 +32,8 @@ use TYPO3\CMS\Core\SingletonInterface;
  * @package    TYPO3
  * @subpackage nws_municipal_statutes
  */
-class UserSession implements SingletonInterface {
+class UserSession implements SingletonInterface
+{
 	/**
 	 * sessionStorage
 	 *
@@ -46,42 +47,9 @@ class UserSession implements SingletonInterface {
 	 * @param SessionStorage $sessionStorage
 	 *
 	 */
-	public function __construct(SessionStorage $sessionStorage) {
+	public function __construct(SessionStorage $sessionStorage)
+	{
 		$this->sessionStorage = $sessionStorage;
-	}
-
-	/**
-	 * Returns the object stored in the session of the user with the choice of page number
-	 *
-	 * @return NULL|object Nwsnet\NwsMunicipalStatutes\Session\SessionStorage getObject()
-	 */
-	public function getBrowse() {
-		if ($this->sessionStorage->has('browse')) {
-			return $this->sessionStorage->getObject('browse');
-		}
-		return NULL;
-	}
-
-	/**
-	 * Saves the object in the session of the user with the choice of page number
-	 *
-	 * @param object $value page number
-	 *
-	 * @return void
-	 */
-	public function saveBrowse(array $value) {
-		$this->sessionStorage->storeObject($value, 'browse');
-	}
-
-	/**
-	 * Deletes the object in the session of the user with the choice of page number
-	 *
-	 * @return void
-	 */
-	public function cleanBrowse() {
-		if ($this->sessionStorage->has('browse')) {
-			$this->sessionStorage->clean('browse');
-		}
 	}
 
 	/**
@@ -89,11 +57,12 @@ class UserSession implements SingletonInterface {
 	 *
 	 * @return NULL|object Nwsnet\NwsMunicipalStatutes\Session\SessionStorage getObject()
 	 */
-	public function getSearch() {
+	public function getSearch()
+	{
 		if ($this->sessionStorage->has('search')) {
 			return $this->sessionStorage->getObject('search');
 		} else {
-			return NULL;
+			return null;
 		}
 	}
 
@@ -104,7 +73,8 @@ class UserSession implements SingletonInterface {
 	 *
 	 * @return void
 	 */
-	public function saveSearch(array $value) {
+	public function saveSearch(array $value)
+	{
 		$this->sessionStorage->storeObject($value, 'search');
 	}
 
@@ -113,44 +83,10 @@ class UserSession implements SingletonInterface {
 	 *
 	 * @return void
 	 */
-	public function cleanSearch() {
+	public function cleanSearch()
+	{
 		if ($this->sessionStorage->has('search')) {
 			$this->sessionStorage->clean('search');
-		}
-	}
-
-	/**
-	 * Returns the object stored in the session of the user with the choice of day selection
-	 *
-	 * @return NULL|object Nwsnet\NwsMunicipalStatutes\Session\SessionStorage getObject()
-	 */
-	public function getSearchDay() {
-		if ($this->sessionStorage->has('searchday')) {
-			return $this->sessionStorage->getObject('searchday');
-		} else {
-			return NULL;
-		}
-	}
-
-	/**
-	 * Saves the object in the session of the user with the choice of day selection
-	 *
-	 * @param object $value day selection
-	 *
-	 * @return void
-	 */
-	public function saveSearchDay(array $value) {
-		$this->sessionStorage->storeObject($value, 'searchday');
-	}
-
-	/**
-	 * Deletes the object in the session of the user with the choice of day selection
-	 *
-	 * @return void
-	 */
-	public function cleanSearchDay() {
-		if ($this->sessionStorage->has('searchday')) {
-			$this->sessionStorage->clean('searchday');
 		}
 	}
 
@@ -159,11 +95,12 @@ class UserSession implements SingletonInterface {
 	 *
 	 * @return NULL|object Nwsnet\NwsMunicipalStatutes\Session\SessionStorage getObject()
 	 */
-	public function getReferrer() {
+	public function getReferrer()
+	{
 		if ($this->sessionStorage->has('referrer')) {
 			return $this->sessionStorage->getObject('referrer');
 		}
-		return NULL;
+		return null;
 	}
 
 	/**
@@ -173,7 +110,8 @@ class UserSession implements SingletonInterface {
 	 *
 	 * @return void
 	 */
-	public function saveReferrer(array $value) {
+	public function saveReferrer(array $value)
+	{
 		$this->sessionStorage->storeObject($value, 'referrer');
 	}
 
@@ -182,112 +120,10 @@ class UserSession implements SingletonInterface {
 	 *
 	 * @return void
 	 */
-	public function cleanReferrer() {
+	public function cleanReferrer()
+	{
 		if ($this->sessionStorage->has('referrer')) {
 			$this->sessionStorage->clean('referrer');
-		}
-	}
-
-	/**
-	 * Returns the object that is stored in the user's session with the selection of the organizer
-	 *
-	 * @return NULL|object Nwsnet\NwsMunicipalStatutes\Session\SessionStorage getObject()
-	 */
-	public function getOrganizer() {
-		if ($this->sessionStorage->has('organizer')) {
-			return $this->sessionStorage->getObject('organizer');
-		}
-		return NULL;
-	}
-
-	/**
-	 * Saves the object in the user's session of the organizer data
-	 *
-	 * @param object $value organizer data
-	 *
-	 * @return void
-	 */
-	public function saveOrganizer(array $value) {
-		$this->sessionStorage->storeObject($value, 'organizer');
-	}
-
-	/**
-	 * Deletes the object in the session of the user of the organizer data
-	 *
-	 * @return void
-	 */
-	public function cleanOrganizer() {
-		if ($this->sessionStorage->has('organizer')) {
-			$this->sessionStorage->clean('organizer');
-		}
-	}
-
-	/**
-	 * Returns the object that is stored in the user's session, whether they have come from the password page
-	 *
-	 * @return NULL|object Nwsnet\NwsMunicipalStatutes\Session\SessionStorage getObject()
-	 */
-	public function getPassword() {
-		if ($this->sessionStorage->has('password')) {
-			return $this->sessionStorage->getObject('password');
-		}
-		return NULL;
-	}
-
-	/**
-	 * Saves the object in the user's session when they come from the password page
-	 *
-	 * @param object $value data password page
-	 *
-	 * @return void
-	 */
-	public function savePassword(array $value) {
-		$this->sessionStorage->storeObject($value, 'password');
-	}
-
-	/**
-	 * Deletes the object in the user's session when they come from the password page
-	 *
-	 * @return void
-	 */
-	public function cleanPassword() {
-		if ($this->sessionStorage->has('password')) {
-			$this->sessionStorage->clean('password');
-		}
-	}
-
-	/**
-	 * Returns the object stored in the session of the user with the choice of month criteria
-	 *
-	 * @return NULL|object Nwsnet\NwsMunicipalStatutes\Session\SessionStorage getObject()
-	 */
-	public function getMonthSelection() {
-		if ($this->sessionStorage->has('month')) {
-			return $this->sessionStorage->getObject('month');
-		} else {
-			return NULL;
-		}
-	}
-
-	/**
-	 * Saves the object in the session of the user with the choice of month criteria
-	 *
-	 * @param object $value search selection
-	 *
-	 * @return void
-	 */
-	public function saveMonthSelection(array $value) {
-		$this->sessionStorage->storeObject($value, 'month');
-	}
-
-	/**
-	 * Deletes the object in the session of the user with the choice of month criteria
-	 *
-	 * @return void
-	 */
-	public function cleanMonthSelection() {
-		if ($this->sessionStorage->has('month')) {
-			$this->sessionStorage->clean('month');
 		}
 	}
 }
