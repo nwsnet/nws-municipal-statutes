9d0882a12c862d1d3ee9ffb6fb323f875a54b17b
Dirk Meinke <dmeinke@die-netzwerkstatt.de>
2019-04-15 13:09:09 +0200

    [TASK][JS] Outsourced call of collapse script

---
+++ Resources/Private/Source/Javascript/jquery.start-collapse.js
@@ -0,0 +1,13 @@
+/**
+ * Project: local law
+ * Dependencies: jQuery,plugin bootstrap collapse
+ * Author: Dirk Meinke
+ * Homepage: http://www.die-netzwerkstatt.de/
+ */
+$(document).ready(function () {
+	if (typeof NwsMunicipalStatutesId !== "undefined") {
+		$('#lg' + NwsMunicipalStatutesId + 'Cont').collapse({
+			show: trueq
+		});
+	}
+});
