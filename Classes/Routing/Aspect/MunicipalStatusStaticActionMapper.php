3871fda60e325d196077bbef92e92325ccd4ac8f
Dirk Meinke <dmeinke@die-netzwerkstatt.de>
2023-07-26 13:28:53 +0200

    [TASK] Added new link title mapper

--- Classes/Routing/Aspect/MunicipalStatusStaticActionMapper.php
+++ Classes/Routing/Aspect/MunicipalStatusStaticActionMapper.php
@@ -23,23 +23,72 @@
 
 namespace Nwsnet\NwsMunicipalStatutes\Routing\Aspect;
 
+use InvalidArgumentException;
 use TYPO3\CMS\Core\Routing\Aspect\PersistedMappableAspectInterface;
 use TYPO3\CMS\Core\Routing\Aspect\StaticMappableAspectInterface;
+use TYPO3\CMS\Extbase\Configuration\Exception\InvalidConfigurationTypeException;
 use TYPO3\CMS\Extbase\Mvc\Exception\InfiniteLoopException;
 use TYPO3\CMS\Extbase\Mvc\Exception\InvalidActionNameException;
 use TYPO3\CMS\Extbase\Mvc\Exception\InvalidControllerNameException;
 use TYPO3\CMS\Extbase\Mvc\Exception\InvalidExtensionNameException;
 
 /**
- * Title of the legislator for the link to the list view
+ * Title of the event for the link to the list view
  *
  * @package    TYPO3
- * @subpackage nws_municipal_statutes
+ * @subpackage nws_council_system
  *
  */
-class NwsLegislatorTitleMapper extends AbstractTitleMapper implements PersistedMappableAspectInterface,
-                                                                      StaticMappableAspectInterface
+class MunicipalStatusStaticActionMapper extends AbstractTitleMapper implements PersistedMappableAspectInterface,
+                                                                               StaticMappableAspectInterface
 {
+    /**
+     * @var string
+     */
+    protected $controller;
+
+    /**
+     * @var string
+     */
+    protected $action;
+
+    /**
+     * @var string
+     */
+    protected $argument;
+
+    /**
+     * MunicipalStatusStaticActionMapper constructor.
+     * @param array $settings
+     * @throws InvalidConfigurationTypeException
+     */
+    public function __construct(array $settings)
+    {
+        $settings['tableName'] = 'notable';
+        $settings['fieldName'] = 'nofield';
+        parent::__construct($settings);
+        $controller = $settings['controller'] ?? null;
+        $action = $settings['action'] ?? null;
+        $argument = $settings['argument'] ?? [];
+        $plugin = $settings['plugin'] ?? null;
+
+        if (!is_string($controller)) {
+            throw new InvalidArgumentException('controller must be string', 1634128608);
+        }
+        if (!is_string($action)) {
+            throw new InvalidArgumentException('action must be be string', 1634128608);
+        }
+        if (!is_string($argument)) {
+            throw new InvalidArgumentException('arguemnt must be string', 1634128608);
+        }
+
+
+        $this->controller = $controller;
+        $this->action = $action;
+        $this->argument = $argument;
+        $this->pluginName = $plugin ?? 'Pi1';
+    }
+
     /**
      * Get the title and generate the link name
      *
@@ -52,32 +101,31 @@ class NwsLegislatorTitleMapper extends AbstractTitleMapper implements PersistedM
      */
     public function generate(string $value): ?string
     {
-        $legislator = (int)$value;
-
-        $cacheKey = 'nws-legislator-slug-' . $legislator;
+        $cacheKey = md5('nws-static-action-mapper-'.$this->controller.$this->action.$this->argument.$value);
         if ($this->cache->has($cacheKey)) {
             return (string)$this->cache->get($cacheKey);
         }
 
         $arguments = array(
-            'controller' => 'LocalLaw',
-            'action' => 'showTitleLegislator',
-            'legislator' => $legislator
+            'controller' => $this->controller,
+            'action' => $this->action,
+            $this->argument => $value,
         );
-        $title = $this->getTitle($arguments);
+        $title = $this->getTitle($arguments, $this->argument);
         if ($title === null) {
-            return (int)$legislator;
+            return $value;
         }
-
+        $title = str_replace('/', '-', $title);
         $slug = $this->slugHelper->sanitize($title);
-        $slug = mb_substr($slug, 0, $this->maxLength) . '-' . $legislator;
+        $slug = mb_substr($slug, 0, $this->maxLength).'-'.$value;
         $slug = $this->slugHelper->sanitize($slug);
-        $this->cache->set($cacheKey, $slug, array('callLocalLawTitleApi'), self::SLUG_CACHE_LIFETIME);
+        $this->cache->set($cacheKey, $slug, array('calRegionalMapApi'), self::SLUG_CACHE_LIFETIME);
+
         return $slug;
     }
 
     /**
-     * Determines the ID and checks if the legislator still exists
+     * Determines the ID and checks if the event still exists
      *
      * @param string $value
      * @return string|null
@@ -88,28 +136,26 @@ class NwsLegislatorTitleMapper extends AbstractTitleMapper implements PersistedM
      */
     public function resolve(string $value): ?string
     {
-        $match = [];
-        if (!preg_match('/^([\p{L}0-9\/-]+-)?(\d+)$/', $value, $match)) {
+        if (!$id = $this->getIdByString($value)) {
             return null;
         }
-        $legislator = (int)$match[2];
-        $cacheKey = 'nws-legislator-slug-' . $legislator;
+        $cacheKey = md5('nws-static-action-mapper-'.$this->controller.$this->action.$this->argument.$value);
         if ($this->cache->has($cacheKey)) {
-            return (int)$legislator;
+            return (string)$id;
         }
 
         // Test if the id actually exists.
         // Should be done to properly display 404 pages, and to avoid cache flooding.
         $arguments = array(
-            'controller' => 'LocalLaw',
-            'action' => 'showTitleLegislator',
-            'legislator' => $legislator
+            'controller' => $this->controller,
+            'action' => $this->action,
+            $this->argument => $id,
         );
-        $title = $this->getTitle($arguments);
+        $title = $this->getTitle($arguments, $this->argument);
         if ($title === null) {
             return null; // -> 404
         }
 
-        return $legislator;
+        return $id;
     }
 }
