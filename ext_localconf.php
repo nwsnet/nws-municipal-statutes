5f7aba80ec8472ed7d4942be65fa74e9827ac666
Dirk Meinke <dmeinke@die-netzwerkstatt.de>
2023-07-26 13:36:43 +0200

    [TASK] Conversion of the configuration settings for TYPO3 11

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,59 +1,83 @@
 <?php
 
-if (!defined('TYPO3_MODE')) {
-    die ('Access denied.');
-}
+defined('TYPO3_MODE') || die('Access denied.');
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'Nwsnet.' . 'nws_municipal_statutes',
-    'Pi1',
-    array(
-        'LocalLaw' => 'list,singlelist,show,showTitle,showTitleLegislator',
-    ),
-    // non-cacheable actions
-    array(
-        'LocalLaw' => 'list,singlelist',
-
-    )
-);
-if (TYPO3_MODE === 'BE') {
-    if (class_exists('\TYPO3\CMS\Core\Imaging\IconRegistry')) {
-        $icons = [
-            'ext-nws-municipal-statutes-wizard-icon' => 'ce_wiz.svg',
-        ];
-        $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-        foreach ($icons as $identifier => $path) {
-            $iconRegistry->registerIcon(
-                $identifier,
-                \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-                ['source' => 'EXT:nws_municipal_statutes/Resources/Public/Icons/' . $path]
+call_user_func(
+    function ($extKey) {
+        $versionAsInt = \TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version);
+        if ($versionAsInt < 9999999) {
+            \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+                'Nwsnet.'.$extKey,
+                'Pi1',
+                [
+                    'LocalLaw' => 'list,singlelist,show,showTitle,showTitleLegislator',
+                ],
+                // non-cacheable actions
+                [
+                    'LocalLaw' => 'list,singlelist',
+                ]
+            );
+        } else {
+            \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+                'Nwsnet.'.$extKey,
+                'Pi1',
+                [
+                    \Nwsnet\NwsMunicipalStatutes\Controller\LocalLawController::class => 'list,singlelist,show,showTitle,showTitleLegislator',
+                ],
+                // non-cacheable actions
+                [
+                    \Nwsnet\NwsMunicipalStatutes\Controller\LocalLawController::class => 'list,singlelist',
+                ]
             );
         }
-    }
-}
-//Add TSConfig
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:nws_municipal_statutes/Configuration/TSConfig/pageTSConfig.ts">');
-//Add Eid
-$GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['nwsMunicipalStatutesDispatcher'] = \Nwsnet\NwsMunicipalStatutes\Eid\Dispatcher::class . '::processRequest';
 
-// Caching framework
-if (!is_array($GLOBALS['TYPO3_CONF_VARS'] ['SYS']['caching']['cacheConfigurations']['nws_municipal_statutes'])) {
-    $GLOBALS['TYPO3_CONF_VARS'] ['SYS']['caching']['cacheConfigurations']['nws_municipal_statutes'] = array();
-    $GLOBALS['TYPO3_CONF_VARS'] ['SYS']['caching']['cacheConfigurations']['nws_municipal_statutes']['frontend'] = 'TYPO3\\CMS\\Core\\Cache\\Frontend\\VariableFrontend';
-    $GLOBALS['TYPO3_CONF_VARS'] ['SYS']['caching']['cacheConfigurations']['nws_municipal_statutes']['groups'] = array('pages');
-    // Cache for 24 hour
-    $GLOBALS['TYPO3_CONF_VARS'] ['SYS']['caching']['cacheConfigurations']['nws_municipal_statutes']['options'] = array('defaultLifetime' => 86400);
-}
-//For providing the title links in Sites Configuration
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['routing']['aspects']['NwsLegislatorTitleMapper'] = \Nwsnet\NwsMunicipalStatutes\Routing\Aspect\NwsLegislatorTitleMapper::class;
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['routing']['aspects']['NwsLegalNormTitleMapper'] = \Nwsnet\NwsMunicipalStatutes\Routing\Aspect\NwsLegalNormTitleMapper::class;
-//Generation of the page title for TYPO3 9.5
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(trim('
-    config.pageTitleProviders {
-        municipal {
-            provider = Nwsnet\NwsMunicipalStatutes\PageTitle\MunicipalPageTitleProvider
-            before = record
-            after = altPageTitle
+        if (TYPO3_MODE === 'BE') {
+            if (class_exists('\TYPO3\CMS\Core\Imaging\IconRegistry')) {
+                $icons = [
+                    'ext-nws-municipal-statutes-wizard-icon' => 'ce_wiz.svg',
+                ];
+                $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+                    \TYPO3\CMS\Core\Imaging\IconRegistry::class
+                );
+                foreach ($icons as $identifier => $path) {
+                    $iconRegistry->registerIcon(
+                        $identifier,
+                        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+                        ['source' => 'EXT:nws_municipal_statutes/Resources/Public/Icons/'.$path]
+                    );
+                }
+            }
         }
-    }
-'));
+
+        //Add TSConfig
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+            '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:nws_municipal_statutes/Configuration/TSConfig/pageTSConfig.ts">'
+        );
+        //Add Eid
+        $GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['nwsMunicipalStatutesDispatcher'] = \Nwsnet\NwsMunicipalStatutes\Eid\Dispatcher::class.'::processRequest';
+
+        // Caching framework
+        if (!is_array($GLOBALS['TYPO3_CONF_VARS'] ['SYS']['caching']['cacheConfigurations'][$extKey] ?? null)) {
+            $GLOBALS['TYPO3_CONF_VARS'] ['SYS']['caching']['cacheConfigurations'][$extKey] = array();
+            $GLOBALS['TYPO3_CONF_VARS'] ['SYS']['caching']['cacheConfigurations'][$extKey]['frontend'] = 'TYPO3\\CMS\\Core\\Cache\\Frontend\\VariableFrontend';
+            $GLOBALS['TYPO3_CONF_VARS'] ['SYS']['caching']['cacheConfigurations'][$extKey]['groups'] = array('pages');
+            // Cache for 24 hour
+            $GLOBALS['TYPO3_CONF_VARS'] ['SYS']['caching']['cacheConfigurations'][$extKey]['options'] = array('defaultLifetime' => 86400);
+        }
+
+        //For providing the title links in Sites Configuration
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['routing']['aspects']['MunicipalStatusStaticActionMapper'] = \Nwsnet\NwsMunicipalStatutes\Routing\Aspect\MunicipalStatusStaticActionMapper::class;
+
+        //Generation of the page title for TYPO3 9.5
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(trim('
+            config.pageTitleProviders {
+                nwsmunicipalstatutes {
+                    provider = Nwsnet\NwsMunicipalStatutes\PageTitle\MunicipalPageTitleProvider
+                    before = record
+                    after = altPageTitle
+				}
+			}
+		'));
+    },
+    'nws_municipal_statutes'
+);
--- ext_tables.php
+++ ext_tables.php
@@ -1,21 +1,30 @@
 <?php
 
-if (!defined('TYPO3_MODE')) {
-    die ('Access denied.');
-}
-$extensionName = \TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase('nws_municipal_statutes');
+defined('TYPO3_MODE') || die('Access denied.');
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    'nws_municipal_statutes',
-    'Pi1',
-    'LLL:EXT:nws_municipal_statutes/Resources/Private/Language/locallang.xlf:tx_nwsmunicipalstatutes_pi1.title'
+call_user_func(
+    function ($extKey) {
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+            $extKey,
+            'Pi1',
+            'LLL:EXT:nws_municipal_statutes/Resources/Private/Language/locallang.xlf:tx_nwsmunicipalstatutes_pi1.title'
+        );
+
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+            $extKey,
+            'Configuration/TypoScript',
+            'Municipal statutes'
+        );
+
+        //Add flexform of the map for pi1
+        $extensionName = \TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($extKey);
+        $pluginSignature = strtolower($extensionName) . '_pi1';
+        $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+        $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'select_key,pages';
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+            $pluginSignature,
+            'FILE:EXT:' . $extKey . '/Configuration/FlexForms/flexform.xml'
+        );
+    },
+    'nws_municipal_statutes'
 );
-
-$pluginSignature = strtolower($extensionName) . '_pi1';
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'select_key,pages';
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature,
-    'FILE:EXT:' . 'nws_municipal_statutes' . '/Configuration/FlexForms/flexform.xml');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('nws_municipal_statutes', 'Configuration/TypoScript',
-    'Municipal statutes');
